04d40e9dee0bd147ccafb3cd3a72c49d5484214e
Sami Ahmed Siddiqui <samiahmedsiddiqui@users.noreply.github.com>
2016-08-24 16:50:56 +0500

    Create interleaving.js

---
+++ jsv4/design-patterns/message-update-delete/interleaving.js
@@ -0,0 +1,13 @@
+pubnub.publish({
+        message: {
+                message_id: 10001,
+                channel: "jasdeep-status",
+                original_timetoken: 
+                user: "jasdeep",
+                status: "Writing up design patterns...",
+                usecase: "update",
+                deleted: false,
+                is_update: true
+        },
+     channel: "jasdeep-status"
+})
